465bf6708e9cab92b8b9741bfb0a003f4c4b4e37
System administrator <root@sancta-gw.>
2025-11-12 17:55:36 +0000

    Add minimal hardware config for CI

---
+++ hosts/sancta-choir/hardware-configuration.nix
@@ -0,0 +1,14 @@
+# Minimal hardware configuration for CI/testing
+# Real hardware config is machine-specific and not committed
+{ modulesPath, ... }:
+{
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+  
+  boot.loader.grub.device = "/dev/sda";
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_blk" ];
+  
+  fileSystems."/" = {
+    device = "/dev/sda1";
+    fsType = "ext4";
+  };
+}
